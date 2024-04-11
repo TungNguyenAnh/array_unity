8584772836cacb780581b149e9af7700f0c5483d
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-11 09:36:51 +0700

    Create TH1.cs

---
+++ TH1.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[5];
+
+numbers[0] = 1;
+numbers[1] = 3;
+numbers[2] = 4;
+numbers[3] = 11;
+numbers[4] = 6;
+
+
+
+int total = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    total = total + numbers[i];
+}
+System.Console.WriteLine(total);
+
