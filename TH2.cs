07191fd9c1703d546e0f93a321bb9c245268be85
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-11 09:52:28 +0700

    TH2

---
+++ TH2.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.WebSockets;
+
+string[] students = { "Christian", "Michael", "Camila",
+ "Sienna", "Tanya", "Connor",
+ "Zachariah", "Mallory", "Zoe",
+  "Emily" };
+
+System.Console.WriteLine("Insert student name: ");
+string input_name = Console.ReadLine();
+
+bool isExist = false;
+
+for (int i = 0; i < students.Length; i++)
+{
+    if (students[i].Equals(input_name))
+    {
+        System.Console.WriteLine("Position of the students in the list " + input_name + "is: " + (i + 1));
+        isExist = true;
+        break;
+    }
+}
+if (!isExist)
+{
+    System.Console.WriteLine("Not found:" + input_name + " in the list!");
+}
